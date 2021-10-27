2d3f76429a68011a4c138e42bfb5a6bea36cdf6d
ExtremeBlackLiu <54531236+extremeblackliu@users.noreply.github.com>
2021-10-28 07:57:47 +0800

    Update Includes.hpp

--- Includes.hpp
+++ Includes.hpp
@@ -20,7 +20,6 @@
 #include "Functions/String.hpp"
 
 // Unity API - Include here so we have access to some cute IL2CPP Features
-#include "Unity/API/_TEMPLATE_API.hpp"
 #include "Unity/API/Object.hpp"
 #include "Unity/API/Component.hpp"
 #include "Unity/API/GameObject.hpp"
